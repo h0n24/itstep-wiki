24c16c144d8043e6bf44a2cb02941f33ad83785b
Jan Šablatura <h0n24.cz@gmail.com>
2022-05-30 07:17:00 +0200

    Correct github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,8 +36,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/h0n24/itstep-wiki/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/h0n24/itstep-wiki/blob/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
